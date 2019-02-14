30abbe194dc61517392989aa44c6d96aab777165
Mark van Duijker <mark@duyker.nl>
2019-02-15 00:36:20 +0100

    :green_heart: Make tests compatible with lower laravel versions

--- tests/Feauture/TransactionalAwareEventsTest.php
+++ tests/Feauture/TransactionalAwareEventsTest.php
@@ -9,130 +9,155 @@ use MVanDuijker\TransactionalModelEvents\Tests\TestCase;
 
 class TransactionalAwareEventsTest extends TestCase
 {
+    private $recordedEvents = [];
+
+    protected function setUp()
+    {
+        parent::setUp();
+        $this->recordedEvents = [];
+    }
+
     /** @test */
     public function it_fires_commit_created()
     {
-        $this->fakeTransactionalEvents(TestModel::class);
+        $this->recordEvents();
 
         DB::beginTransaction();
         TestModel::create(['name' => 'test create']);
-        Event::assertNotDispatched('eloquent.afterCommit.created: ' . TestModel::class);
+        $this->assertNotDispatched('eloquent.afterCommit.created: ' . TestModel::class);
         DB::commit();
 
-        Event::assertDispatched('eloquent.afterCommit.created: ' . TestModel::class);
+        $this->assertDispatched('eloquent.afterCommit.created: ' . TestModel::class);
     }
 
     /** @test */
     public function it_fires_commit_saved()
     {
-        $this->fakeTransactionalEvents(TestModel::class);
+        $this->recordEvents();
 
         DB::beginTransaction();
         TestModel::create(['name' => 'test saved']);
-        Event::assertNotDispatched('eloquent.afterCommit.saved: ' . TestModel::class);
+        $this->assertNotDispatched('eloquent.afterCommit.saved: ' . TestModel::class);
         DB::commit();
 
-        Event::assertDispatched('eloquent.afterCommit.saved: ' . TestModel::class);
+        $this->assertDispatched('eloquent.afterCommit.saved: ' . TestModel::class);
     }
 
     /** @test */
     public function it_fires_commit_updated()
     {
-        $this->fakeTransactionalEvents(TestModel::class);
+        $this->recordEvents();
         $model = TestModel::create(['name' => 'test saved']);
 
         DB::beginTransaction();
         $model->update(['name' => 'new name']);
-        Event::assertNotDispatched('eloquent.afterCommit.updated: ' . TestModel::class);
+        $this->assertNotDispatched('eloquent.afterCommit.updated: ' . TestModel::class);
         DB::commit();
 
-        Event::assertDispatched('eloquent.afterCommit.updated: ' . TestModel::class);
+        $this->assertDispatched('eloquent.afterCommit.updated: ' . TestModel::class);
     }
 
     /** @test */
     public function it_fires_commit_deleted()
     {
-        $this->fakeTransactionalEvents(TestModel::class);
+        $this->recordEvents();
         $model = TestModel::create(['name' => 'test delete']);
 
         DB::beginTransaction();
         $model->delete();
-        Event::assertNotDispatched('eloquent.afterCommit.deleted: ' . TestModel::class);
+        $this->assertNotDispatched('eloquent.afterCommit.deleted: ' . TestModel::class);
         DB::commit();
 
-        Event::assertDispatched('eloquent.afterCommit.deleted: ' . TestModel::class);
+        $this->assertDispatched('eloquent.afterCommit.deleted: ' . TestModel::class);
     }
 
     /** @test */
     public function it_fires_rollback_created()
     {
-        $this->fakeTransactionalEvents(TestModel::class);
+        $this->recordEvents();
 
         DB::beginTransaction();
         TestModel::create(['name' => 'test create']);
-        Event::assertNotDispatched('eloquent.afterRollback.created: ' . TestModel::class);
+        $this->assertNotDispatched('eloquent.afterRollback.created: ' . TestModel::class);
         DB::rollBack();
 
-        Event::assertDispatched('eloquent.afterRollback.created: ' . TestModel::class);
+        $this->assertDispatched('eloquent.afterRollback.created: ' . TestModel::class);
     }
 
     /** @test */
     public function it_fires_rollback_saved()
     {
-        $this->fakeTransactionalEvents(TestModel::class);
+        $this->recordEvents();
 
         DB::beginTransaction();
         TestModel::create(['name' => 'test saved']);
-        Event::assertNotDispatched('eloquent.afterRollback.saved: ' . TestModel::class);
+        $this->assertNotDispatched('eloquent.afterRollback.saved: ' . TestModel::class);
         DB::rollBack();
 
-        Event::assertDispatched('eloquent.afterRollback.saved: ' . TestModel::class);
+        $this->assertDispatched('eloquent.afterRollback.saved: ' . TestModel::class);
     }
 
     /** @test */
     public function it_fires_rollback_updated()
     {
-        $this->fakeTransactionalEvents(TestModel::class);
+        $this->recordEvents();
         $model = TestModel::create(['name' => 'test saved']);
 
         DB::beginTransaction();
         $model->update(['name' => 'new name']);
-        Event::assertNotDispatched('eloquent.afterRollback.updated: ' . TestModel::class);
+        $this->assertNotDispatched('eloquent.afterRollback.updated: ' . TestModel::class);
         DB::rollBack();
 
-        Event::assertDispatched('eloquent.afterRollback.updated: ' . TestModel::class);
+        $this->assertDispatched('eloquent.afterRollback.updated: ' . TestModel::class);
     }
 
     /** @test */
     public function it_fires_rollback_deleted()
     {
-        $this->fakeTransactionalEvents(TestModel::class);
+        $this->recordEvents();
         $model = TestModel::create(['name' => 'test delete']);
 
         DB::beginTransaction();
         $model->delete();
-        Event::assertNotDispatched('eloquent.afterRollback.deleted: ' . TestModel::class);
+        $this->assertNotDispatched('eloquent.afterRollback.deleted: ' . TestModel::class);
         DB::rollBack();
 
-        Event::assertDispatched('eloquent.afterRollback.deleted: ' . TestModel::class);
+        $this->assertDispatched('eloquent.afterRollback.deleted: ' . TestModel::class);
     }
 
     public function test_it_fires_with_multiple_models()
     {
-        $this->fakeTransactionalEvents(TestModel::class);
+        $this->recordEvents();
 
         DB::beginTransaction();
         TestModel::create(['name' => 'test create first']);
         TestModel::create(['name' => 'test create second']);
         DB::rollBack();
 
-        Event::assertDispatchedTimes('eloquent.afterRollback.created: ' . TestModel::class, 2);
+        $this->assertDispatchedTimes('eloquent.afterRollback.created: ' . TestModel::class, 2);
     }
 
-    private function fakeTransactionalEvents($className)
+    private function recordEvents()
     {
-        Event::fake(array_map(function ($eventName) use ($className) {
-            return "eloquent.$eventName: " . $className;
-        }, $this->transactionalEvents()));
+        Event::listen('eloquent.*', function ($eventName, $_) {
+            $this->recordedEvents[] = $eventName;
+        });
+    }
+
+    private function assertDispatched($event)
+    {
+        $this->assertTrue(in_array($event, $this->recordedEvents), "$event not dispatched");
+    }
+
+    private function assertNotDispatched($event)
+    {
+        $this->assertFalse(in_array($event, $this->recordedEvents), "$event dispatched");
+    }
+
+    private function assertDispatchedTimes($event, $times = 1)
+    {
+        $this->assertCount($times, array_filter($this->recordedEvents, function ($recordedEvent) use ($event) {
+            return $recordedEvent === $event;
+        }));
     }
 }
