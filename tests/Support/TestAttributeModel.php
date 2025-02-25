114c13a555d72c10ce4995118563194e97d53150
Mark van Duijker <mark@duyker.nl>
2025-02-25 23:41:29 +0100

    refactor: improve tests

--- tests/Support/TestAttributeModel.php
+++ tests/Support/TestAttributeModel.php
@@ -3,8 +3,14 @@
 namespace MVanDuijker\TransactionalModelEvents\Tests\Support;
 
 use Illuminate\Database\Eloquent\Attributes\ObservedBy;
+use MVanDuijker\TransactionalModelEvents\TransactionalAwareEvents;
 
 #[ObservedBy(TestObserver::class)]
 class TestAttributeModel extends TestModel
 {
+    use TransactionalAwareEvents;
+
+    protected $table = 'test_models';
+    protected $guarded = [];
+    public $timestamps = false;
 }
--- tests/Support/TestObserver.php
+++ tests/Support/TestObserver.php
@@ -4,17 +4,17 @@ namespace MVanDuijker\TransactionalModelEvents\Tests\Support;
 
 class TestObserver
 {
-    public function afterCommitCreated(TestModel $testModel)
+    public function afterCommitCreated(TestModel|TestAttributeModel $testModel)
     {
         $testModel->observer_call_created = true;
     }
 
-    public function afterCommitSaved(TestModel $testModel)
+    public function afterCommitSaved(TestModel|TestAttributeModel $testModel)
     {
         $testModel->observer_call_saved = true;
     }
 
-    public function afterRollbackSaved(TestModel $testModel)
+    public function afterRollbackSaved(TestModel|TestAttributeModel $testModel)
     {
         $testModel->observer_call_rollback_saved = true;
     }
